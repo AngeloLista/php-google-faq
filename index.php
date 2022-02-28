76528a9d62fa18dcdaf7958989c0338c6a25c9dd
AngeloLista <abstractplayer@gmail.com>
2022-02-28 20:22:06 +0100

    fixed lists style margin and padding

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ $faqs = [
         <ol>
             <li>
                 La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-                <ol>
+                <ol type=\"a\">
                     <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
                     <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
                 </ol>
